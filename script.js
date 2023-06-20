d76b5c8fe1754502c8dc270949f325526ab7e15b
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 02:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function remove(){
+	const parent = document.getElementById("colorSelect")
+	let curr= parent.value
+	let children= parent.children
+	console.log(children[0])
+	for(let i=0;i<children.length;i++){
+		if(children[i].innerText===curr){
+			children[i].remove()
+		}
+	}
+}
